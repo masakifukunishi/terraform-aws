24d6349447b7955b4038d127fd27cac1b3968081
fukunishi <fukunishi@hashhub.tokyo>
2024-01-22 18:24:44 +0900

    add security group

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "this" {
+  ami           = "ami-0b828c1c5ac3f13ee"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.this.id]
+
+  tags = {
+    Name = "udemy-terraform-ec2"
+  }
+}
+
+resource "aws_security_group" "this" {
+  name = "udemy-terraform-ec2-sg"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
